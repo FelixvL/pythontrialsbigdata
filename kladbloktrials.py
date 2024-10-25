ae424bb6447719d5825cbe93426bb384eb7b1498
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-10-25 16:01:46 +0200

    grafieken wegschrijvend naar losse png's

--- kladbloktrials.py
+++ kladbloktrials.py
@@ -1,5 +1,7 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import re  # Voor het opschonen van de bestandsnaam
 
 # 1. Laad het MeasureCodes.csv bestand
 df_measure_codes = pd.read_csv('docs/MeasureCodes.csv', sep=";")
@@ -20,10 +22,6 @@ df_perioden = pd.read_csv('docs/periodecodes.csv', sep=";")
 # Koppel de consumentenbestedingen met de periodecodes op basis van 'Perioden' en 'Identifier'
 df_consumentenbestedingen_grouped = df_consumentenbestedingen.merge(df_perioden[['Identifier', 'Title']], left_on='Perioden', right_on='Identifier')
 
-# Controleer de kolommen die de jaartallen bevatten
-print("Kolommen in consumentenbestedingen dataset:")
-print(df_consumentenbestedingen_grouped.columns)
-
 # 5. Laad de inflatiegegevens
 df_inflatie = pd.read_csv('docs/inflatie.csv', sep=";")
 
@@ -39,18 +37,19 @@ df_inflatie_yearly = df_inflatie.groupby('Jaar')['Value'].mean().reset_index()
 # Gebruik nu de juiste kolom om te mergen, in dit geval Title_y (controleer of het jaar in Title_x of Title_y zit)
 df_merged = df_consumentenbestedingen_grouped.merge(df_inflatie_yearly, left_on='Title_y', right_on='Jaar')
 
-# 6. Ga verder met de sectoraal gerichte analyse zoals gepland
-# Groepeer de data per sector en per jaar
+# 6. Groepeer de data per sector en per jaar
 df_sectoral = df_merged.groupby(['Title_x', 'Jaar'])[['Value_x', 'Value_y']].sum().reset_index()
 
-# Controleer het gegroepeerde dataframe
-print("\nGegroepeerde data per sector:")
-print(df_sectoral.head())
+# Maak een map voor de PNG-bestanden als die nog niet bestaat
+os.makedirs("plots", exist_ok=True)
 
-# Correlatieanalyse per sector
+# Correlatieanalyse per sector en sla op als PNG
 sectors = df_sectoral['Title_x'].unique()
 
 for sector in sectors:
+    # Maak de bestandsnaam schoon door ongewenste tekens te vervangen
+    safe_sector_name = re.sub(r'[<>:"/\\|?*]', '_', sector)
+    
     df_sector = df_sectoral[df_sectoral['Title_x'] == sector]
     correlatie = df_sector[['Value_x', 'Value_y']].corr()
     print(f"\nCorrelatie voor {sector}:")
@@ -63,4 +62,9 @@ for sector in sectors:
     plt.xlabel('Inflatie (%)')
     plt.ylabel('Consumentenbestedingen (in miljoenen euro)')
     plt.grid(True)
-    plt.show()
+
+    # Sla elke grafiek op als PNG-bestand in de 'plots' map
+    plt.savefig(f"plots/{safe_sector_name}.png")
+    plt.close()  # Sluit de figuur om geheugen te besparen
+
+print("Alle grafieken zijn opgeslagen in de 'plots' map.")
