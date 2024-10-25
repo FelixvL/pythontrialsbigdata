03505d02ce6e42a11fd4e837bdb0c8d5b985e300
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-10-25 15:04:03 +0200

    grafiek goed per jaar

--- kladbloktrials.py
+++ kladbloktrials.py
@@ -4,25 +4,17 @@ import matplotlib.pyplot as plt
 # Het Observations.csv-bestand laden
 df_observations = pd.read_csv('docs/Observations.csv', sep=";")
 
-# De eerste paar rijen van de data bekijken
-print(df_observations.head())
-
-# Informatie over de data weergeven (zoals kolommen, datatypes)
-print(df_observations.info())
-
-print("---")
-df_perioden = pd.read_csv('docs/PeriodenCodes.csv', sep=";")
-
-# De eerste paar rijen van de PeriodenCodes bekijken
-print(df_perioden.head())
-
-# Zorg ervoor dat de 'Value' kolom wordt omgezet naar een numeriek datatype (verwijder eerst de komma's)
+# Data opschonen: vervang komma's en zet om naar numeriek
 df_observations['Value'] = df_observations['Value'].str.replace(',', '.').astype(float)
 
-# Groepeer de gegevens per jaar en sommeer de bestedingen
-df_observations_grouped = df_observations.groupby('Perioden')['Value'].sum().reset_index()
+# Filter om alleen volledige jaren (bijv. 1996JJ00) te selecteren
+df_observations_yearly = df_observations[df_observations['Perioden'].str.endswith('JJ00')]
 
-# Voeg een leesbare tijdsperiode toe vanuit PeriodenCodes.csv (gebruik de juiste kolomnaam 'Identifier' om te koppelen)
+# Groepeer de gegevens per jaar en sommeer de bestedingen
+df_observations_grouped = df_observations_yearly.groupby('Perioden')['Value'].sum().reset_index()
+
+# Voeg een leesbare tijdsperiode toe vanuit PeriodenCodes.csv
+df_perioden = pd.read_csv('docs/PeriodenCodes.csv', sep=";")
 df_observations_grouped = df_observations_grouped.merge(df_perioden[['Identifier', 'Title']], left_on='Perioden', right_on='Identifier')
 
 # Plot de data
@@ -31,6 +23,9 @@ plt.plot(df_observations_grouped['Title'], df_observations_grouped['Value'], mar
 plt.title('Ontwikkeling van Consumentenbestedingen per Jaar')
 plt.xlabel('Jaar')
 plt.ylabel('Totale Bestedingen (in miljoenen euro)')
-plt.xticks(rotation=45)
+
+# Minder x-as ticks laten zien voor een betere leesbaarheid
+plt.xticks(rotation=45, ticks=df_observations_grouped['Title'][::2])  # Om de labels elke 2 jaar te tonen
 plt.grid(True)
+plt.tight_layout()
 plt.show()
