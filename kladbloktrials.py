a66e0a2d28410c9c548c5bdbb5c385706570b790
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2024-10-25 15:55:27 +0200

    grafieken per sector oppoppend

--- kladbloktrials.py
+++ kladbloktrials.py
@@ -1,26 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# 1. Laad de consumentenbestedingen gegevens
+# 1. Laad het MeasureCodes.csv bestand
+df_measure_codes = pd.read_csv('docs/MeasureCodes.csv', sep=";")
+
+# 2. Laad de consumentenbestedingen gegevens
 df_consumentenbestedingen = pd.read_csv('docs/consumentenbestedingen.csv', sep=";")
 
 # Zet de 'Value' kolom om naar numeriek (vervang komma door punt)
 df_consumentenbestedingen['Value'] = df_consumentenbestedingen['Value'].str.replace(',', '.').astype(float)
 
-# Groepeer de gegevens per jaar en sommeer de bestedingen
-df_consumentenbestedingen_grouped = df_consumentenbestedingen.groupby('Perioden')['Value'].sum().reset_index()
+# 3. Koppel de Measure codes aan de consumentenbestedingen dataset
+df_consumentenbestedingen = df_consumentenbestedingen.merge(df_measure_codes[['Identifier', 'Title']], 
+                                                            left_on='Measure', right_on='Identifier')
 
-# 2. Laad de periodecodes om leesbare jaartallen toe te voegen
+# 4. Laad de periodecodes om leesbare jaartallen toe te voegen
 df_perioden = pd.read_csv('docs/periodecodes.csv', sep=";")
 
 # Koppel de consumentenbestedingen met de periodecodes op basis van 'Perioden' en 'Identifier'
-df_consumentenbestedingen_grouped = df_consumentenbestedingen_grouped.merge(df_perioden[['Identifier', 'Title']], left_on='Perioden', right_on='Identifier')
+df_consumentenbestedingen_grouped = df_consumentenbestedingen.merge(df_perioden[['Identifier', 'Title']], left_on='Perioden', right_on='Identifier')
 
-# Controleer of de data correct is ingeladen
-print("Gecombineerde consumentenbestedingen dataset:")
-print(df_consumentenbestedingen_grouped.head())
+# Controleer de kolommen die de jaartallen bevatten
+print("Kolommen in consumentenbestedingen dataset:")
+print(df_consumentenbestedingen_grouped.columns)
 
-# 3. Laad de inflatiegegevens
+# 5. Laad de inflatiegegevens
 df_inflatie = pd.read_csv('docs/inflatie.csv', sep=";")
 
 # Vervang komma's door punten en zet de Value-kolom om naar numeriek
@@ -32,39 +36,31 @@ df_inflatie['Jaar'] = df_inflatie['Perioden'].str[:4]
 # Bereken de gemiddelde inflatie per jaar
 df_inflatie_yearly = df_inflatie.groupby('Jaar')['Value'].mean().reset_index()
 
-# Controleer de inflatie dataset
-print("Inflatie per jaar:")
-print(df_inflatie_yearly.head())
+# Gebruik nu de juiste kolom om te mergen, in dit geval Title_y (controleer of het jaar in Title_x of Title_y zit)
+df_merged = df_consumentenbestedingen_grouped.merge(df_inflatie_yearly, left_on='Title_y', right_on='Jaar')
 
-# 4. Koppel de consumentenbestedingen met de inflatiegegevens op basis van het jaar ('Title' in consumentenbestedingen en 'Jaar' in inflatie)
-df_merged = df_consumentenbestedingen_grouped.merge(df_inflatie_yearly, left_on='Title', right_on='Jaar')
+# 6. Ga verder met de sectoraal gerichte analyse zoals gepland
+# Groepeer de data per sector en per jaar
+df_sectoral = df_merged.groupby(['Title_x', 'Jaar'])[['Value_x', 'Value_y']].sum().reset_index()
 
-# 5. Filter outliers (consumentenbestedingen boven 10.000 en inflatie boven 10%) direct na het mergen
-df_filtered = df_merged[(df_merged['Value_x'] < 10000) & (df_merged['Value_y'] < 10)]
+# Controleer het gegroepeerde dataframe
+print("\nGegroepeerde data per sector:")
+print(df_sectoral.head())
 
-# Controleer de gefilterde dataset
-print("Gecombineerde dataset zonder outliers:")
-print(df_filtered.head())
+# Correlatieanalyse per sector
+sectors = df_sectoral['Title_x'].unique()
 
-# 6. Correlatieanalyse na filtering van outliers
-correlatie_filtered = df_filtered[['Value_x', 'Value_y']].corr()
-print("\nCorrelatie na filtering van outliers:")
-print(correlatie_filtered)
+for sector in sectors:
+    df_sector = df_sectoral[df_sectoral['Title_x'] == sector]
+    correlatie = df_sector[['Value_x', 'Value_y']].corr()
+    print(f"\nCorrelatie voor {sector}:")
+    print(correlatie)
 
-# 7. Plot de relatie tussen inflatie en consumentenbestedingen (zonder outliers)
-plt.figure(figsize=(8,5))
-plt.scatter(df_filtered['Value_y'], df_filtered['Value_x'])
-plt.title('Relatie tussen Consumentenbestedingen en Inflatie (zonder outliers)')
-plt.xlabel('Inflatie (%)')
-plt.ylabel('Consumentenbestedingen (in miljoenen euro)')
-plt.grid(True)
-plt.show()
-
-# 8. Inspecteer de outliers (extreme waarden die zijn verwijderd)
-outliers_bestedingen = df_merged[df_merged['Value_x'] > 10000]
-print("\nJaren met extreme consumentenbestedingen (boven 10.000 miljoen euro):")
-print(outliers_bestedingen[['Title', 'Value_x', 'Value_y']])
-
-outliers_inflatie = df_merged[df_merged['Value_y'] > 10]
-print("\nJaren met hoge inflatie (boven 10%):")
-print(outliers_inflatie[['Title', 'Value_x', 'Value_y']])
+    # Scatterplot voor deze sector
+    plt.figure(figsize=(8,5))
+    plt.scatter(df_sector['Value_y'], df_sector['Value_x'])
+    plt.title(f'Relatie tussen Consumentenbestedingen en Inflatie ({sector})')
+    plt.xlabel('Inflatie (%)')
+    plt.ylabel('Consumentenbestedingen (in miljoenen euro)')
+    plt.grid(True)
+    plt.show()
